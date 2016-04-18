4dab01442ca6bc386291466fd55646dc7adaf150
Jesú Márquez <jesumarquez@gmail.com>
2016-04-18 16:27:58 -0300

    router api products

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var app = express();
+var express     = require('express'),
+    app         = express(),
+    router      = express.Router(),
+    bodyParser   = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+router.get('/products', function (req, res) {
+    res.json([{
+        name: 'product 1'
+    }]);
+});
+
+app.use('/api', router);
 
 var port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
 
 app.listen(port, function(){
    console.log('Server is running on port: ' + port); 
